3b38ea4e04af092b98b98ac3900777c03734b89c
Kim Desrosiers <kimdesro@gmail.com>
2017-03-16 11:19:27 -0400

    Add pyqt5 to plot matplotlib's graphs and matplotlibrc to configure matplotlib.

--- regression.py
+++ regression.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import quandl
 import math
+import datetime
 import numpy as np
 from sklearn import preprocessing, cross_validation, svm
 from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
 
 df = quandl.get('WIKI/GOOGL')
 
@@ -18,17 +23,43 @@ df.fillna(-99999, inplace=True)
 forecast_out = int(math.ceil(0.01 * len(df)))
 
 df['label'] = df[forecast_col].shift(-forecast_out)
-df.dropna(inplace=True)
 
 X = np.array(df.drop(['label'], 1))
-y = np.array(df['label'])
 X = preprocessing.scale(X)
+X_lately = X[-forecast_out:]
+X = X[:-forecast_out]
+df.dropna(inplace=True)
+y = np.array(df['label'])
 y = np.array(df['label'])
 
 X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
 
+
 for k in ['linear', 'poly', 'rbf', 'sigmoid']:
-    clf = svm.SVR(kernel=k)
+    clf = svm.SVR(kernel='linear')
     clf.fit(X_train, y_train)
     confidence = clf.score(X_test, y_test)
     print(k, confidence)
+
+    forecast_set = clf.predict(X_lately)
+
+    print(forecast_set, confidence, forecast_out)
+
+    df['Forecast'] = np.nan
+
+    last_date = df.iloc[-1].name
+    last_unix = last_date.timestamp()
+    one_day = 86400
+    next_unix = last_unix + one_day
+
+    for i in forecast_set:
+        next_date = datetime.datetime.fromtimestamp(next_unix)
+        next_unix += one_day
+        df.loc[next_date] = [np.nan for _ in range(len(df.columns) - 1)] + [i]
+
+    df['Adj. Close'].plot()
+    df['Forecast'].plot()
+    plt.legend(loc=4)
+    plt.xlabel('Date')
+    plt.ylabel('Price')
+    plt.show()
