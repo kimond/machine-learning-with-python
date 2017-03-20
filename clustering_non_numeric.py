bcca241f94a11b9a2a984f93c41227ca76eaf803
Kim Desrosiers <kimdesro@gmail.com>
2017-03-20 15:35:08 -0400

    Finish the clustering with non numeric data.

--- clustering_non_numeric.py
+++ clustering_non_numeric.py
@@ -52,6 +52,24 @@ def handle_non_numerical_data(df):
             df[column] = list(map(convert_to_int, df[column]))
     return df
 
-df = handle_non_numerical_data(df)
-print(df.head())
 
+df = handle_non_numerical_data(df)
+
+df.drop(['boat'], 1, inplace=True)
+
+X = np.array(df.drop(['survived'], 1).astype(float))
+X = preprocessing.scale(X)
+y = np.array(df['survived'])
+
+clf = KMeans(n_clusters=2)
+clf.fit(X)
+
+correct = 0
+for i in range(len(X)):
+    predict_me = np.array(X[i].astype(float))
+    predict_me = predict_me.reshape(-1, len(predict_me))
+    prediction = clf.predict(predict_me)
+    if prediction[0] == y[i]:
+        correct += 1
+
+print(correct / len(X))
