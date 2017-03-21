320a08a6b2bad3d96d3b3ecb6570adf7903dec3e
Kim Desrosiers <kimdesro@gmail.com>
2017-03-21 11:03:32 -0400

    Add dynamically weighted bandwidth for Mean Shift

--- mean_shift_from_scratch.py
+++ mean_shift_from_scratch.py
@@ -4,15 +4,19 @@ from matplotlib import style
 style.use('ggplot')
 import numpy as np
 
-X = np.array([[1, 2],
-              [1.5, 1.8],
-              [5, 8],
-              [8, 8],
-              [1, 0.6],
-              [9, 11],
-              [8, 2],
-              [10, 2],
-              [9, 3], ])
+from sklearn.datasets.samples_generator import make_blobs
+
+X, y = make_blobs(n_samples=20, centers=3, n_features=2)
+
+# X = np.array([[1, 2],
+#               [1.5, 1.8],
+#               [5, 8],
+#               [8, 8],
+#               [1, 0.6],
+#               [9, 11],
+#               [8, 2],
+#               [10, 2],
+#               [9, 3], ])
 
 # plt.scatter(X[:, 0], X[:, 1], s=150)
 # plt.show()
@@ -21,11 +25,18 @@ colors = 10 * ["g", "r", "c", "b", "k"]
 
 
 class MeanShift:
-    def __init__(self, radius=4):
+    def __init__(self, radius=None, radius_norm_step=100):
         self.radius = radius
+        self.radius_norm_step = radius_norm_step
 
     def fit(self, data):
+        if self.radius is None:
+            all_data_centroid = np.average(data, axis=0)
+            all_data_norm = np.linalg.norm(all_data_centroid)
+            self.radius = all_data_norm / self.radius_norm_step
+
         centroids = {}
+        weights = [i for i in range(self.radius_norm_step)][::-1]
 
         for i in range(len(data)):
             centroids[i] = data[i]
@@ -35,14 +46,36 @@ class MeanShift:
             for i in centroids:
                 in_bandwidth = []
                 centroid = centroids[i]
+
                 for featureset in data:
-                    if np.linalg.norm(featureset - centroid) < self.radius:
-                        in_bandwidth.append(featureset)
+                    distance = np.linalg.norm(featureset - centroid)
+                    if distance == 0:
+                        distance = 0.000000000001
+                    weight_index = int(distance / self.radius)
+                    if weight_index > self.radius_norm_step - 1:
+                        weight_index = self.radius_norm_step - 1
+                    to_add = (weights[weight_index]*2) * [featureset]
+                    in_bandwidth += to_add
 
                 new_centroid = np.average(in_bandwidth, axis=0)
                 new_centroids.append(tuple(new_centroid))
 
             uniques = sorted(list(set(new_centroids)))
+
+            to_pop = []
+            for i in uniques:
+                for ii in uniques:
+                    if i == ii:
+                        pass
+                    elif np.linalg.norm(np.array(i) - np.array(ii)) <= self.radius:
+                        to_pop.append(ii)
+                        break
+
+            for i in to_pop:
+                try:
+                    uniques.remove(i)
+                except:
+                    pass
 
             prev_centroids = dict(centroids)
 
@@ -62,15 +95,32 @@ class MeanShift:
 
         self.centroids = centroids
 
+        self.classifications = {}
+
+        for i in range(len(self.centroids)):
+            self.classifications[i] = []
+
+        for featureset in data:
+            distances = [np.linalg.norm(featureset - self.centroids[centroid]) for centroid in self.centroids]
+            classification = distances.index(min(distances))
+            self.classifications[classification].append(featureset)
+
     def predict(self, data):
-        pass
+        distances = [np.linalg.norm(data - self.centroids[centroid]) for centroid in self.centroids]
+        classification = distances.index(min(distances))
+        return classification
+
 
 clf = MeanShift()
 clf.fit(X)
 
 plt.scatter(X[:, 0], X[:, 1], s=150)
 
+for classification in clf.classifications:
+    color = colors[classification]
+    for featureset in clf.classifications[classification]:
+        plt.scatter(featureset[0], featureset[1], color=color, marker='*', s=150, linewidths=5)
+
 for c in clf.centroids:
     plt.scatter(clf.centroids[c][0], clf.centroids[c][1], marker='*', s=150)
 plt.show()
-
